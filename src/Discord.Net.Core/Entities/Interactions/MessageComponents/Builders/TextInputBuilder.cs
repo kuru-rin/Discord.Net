8686102d87b738d74d6787f43ea7f8178d933826
Mihail Gribkov <61027276+Misha-133@users.noreply.github.com>
2025-04-27 00:31:08 +0300

    Reapply "[Feature] Components V2 (#3065)" (#3100) (#3104)
    This reverts commit d27bb49b2d27d53e42b5b300fd0c618938c6e455.

--- src/Discord.Net.Core/Entities/Interactions/MessageComponents/Builders/TextInputBuilder.cs
+++ src/Discord.Net.Core/Entities/Interactions/MessageComponents/Builders/TextInputBuilder.cs
@@ -6,8 +6,10 @@ namespace Discord;
 /// <summary>
 ///     Represents a builder for creating a <see cref="TextInputComponent"/>.
 /// </summary>
-public class TextInputBuilder
+public class TextInputBuilder : IInteractableComponentBuilder
 {
+    public ComponentType Type => ComponentType.TextInput;
+
     /// <summary>
     ///     The max length of a <see cref="TextInputComponent.Placeholder"/>.
     /// </summary>
@@ -103,6 +105,8 @@ public class TextInputBuilder
     /// </summary>
     public bool? Required { get; set; }
 
+    public int? Id { get; set; }
+
     /// <summary>
     ///     Gets or sets the default value of the text input.
     /// </summary>
@@ -144,7 +148,7 @@ public class TextInputBuilder
     /// <param name="maxLength">The text input's maximum length.</param>
     /// <param name="required">The text input's required value.</param>
     public TextInputBuilder(string label, string customId, TextInputStyle style = TextInputStyle.Short, string placeholder = null,
-        int? minLength = null, int? maxLength = null, bool? required = null, string value = null)
+        int? minLength = null, int? maxLength = null, bool? required = null, string value = null, int? id = null)
     {
         Label = label;
         Style = style;
@@ -154,6 +158,7 @@ public class TextInputBuilder
         MaxLength = maxLength;
         Required = required;
         Value = value;
+        Id = id;
     }
 
     /// <summary>
@@ -261,6 +266,8 @@ public class TextInputBuilder
         if (Style is TextInputStyle.Short && Value?.Any(x => x == '\n') is true)
             throw new ArgumentException($"Value must not contain new line characters when style is {TextInputStyle.Short}.", nameof(Value));
 
-        return new TextInputComponent(CustomId, Label, Placeholder, MinLength, MaxLength, Style, Required, Value);
+        return new TextInputComponent(CustomId, Label, Placeholder, MinLength, MaxLength, Style, Required, Value, Id);
     }
+
+    IMessageComponent IMessageComponentBuilder.Build() => Build();
 }
